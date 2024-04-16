3aa5699e30066dd7217143a72434ecb585d5eafd
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-04-16 13:22:38 +0300

    Updated project. Updated HomeScreen.kt. Updated fun TrendingTopics(). Added LazyRow(). Added TrendingTopic(). Added Spacer().

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -109,6 +112,25 @@ private fun TrendingTopics(
                 )
             }
             Spacer(modifier = Modifier.height(8.dp))
+            LazyRow(
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    top = 8.dp,
+                    end = 16.dp
+                ),
+                content = {
+                    itemsIndexed(
+                        items = trendingTopics,
+                        itemContent = { index,
+                                        trendingModel ->
+                            TrendingTopic(trendingModel)
+                            if (index != trendingTopics.lastIndex) {
+                                Spacer(modifier = Modifier.width(8.dp))
+                            }
+                        }
+                    )
+                }
+            )
         }
     }
 }
